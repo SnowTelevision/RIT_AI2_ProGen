bf76cdcad8fac8800f026343065e02639d713d48
Peng <pguan100@gmail.com>
2017-10-29 22:14:00 -0400

    Start working on combined wave noise.

---
+++ Assets/Scripts/PerlinTest.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PerlinTest : MonoBehaviour
+{
+
+    //EPISODE 2
+
+
+
+    [Header("PREFRENCES")]
+    public bool Randomized = true;      //BOOL NOT IN TUTORIAL {Do you want the terrain generated each time?}
+    public bool Animate = true;         //BOOL NOT IN TUTORIAL {Do you want it to be animated and move?
+    public float Speed = 5;             //FLOAT NOT IN TUTORIAL {If animating, how fast?)
+    //if Animate is true, it works with colliders (add a rigid body to a cube and place the cube above terrain)
+
+    [Space]
+
+    public int depth = 20;  //height from above
+
+    public int width = 256;     //make a int named width and set it to a default of 256
+    public int height = 256;    //make int named height and set it to a default of 256 [Length of terrain]
+
+    public float Scale = 20;
+
+    public float offsetX = 100;
+    public float offsetY = 100;
+
+
+    public void Start()
+    {
+        offsetX = Random.Range(0, 99999);
+        offsetY = Random.Range(0, 99999);
+
+        Terrain terrain = GetComponent<Terrain>();      //for Terrain Data
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+
+    }
+
+
+    void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();      //for Terrain Data
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+
+        if (Animate == true)
+        {
+            offsetX += Time.deltaTime * Speed;
+        }
+    }
+
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width;
+
+        terrainData.size = new Vector3(width, depth, height);
+
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        //terrainData.SetHeights(0, 0, GetComponent<ProcedureArray2D>().map);
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);      //generate some perlin noise value
+            }
+        }
+
+        //string raw = "";
+        //for (int y = 0; y < width; y++)
+        //{
+
+        //    for (int x = 0; x < width; x++)
+        //    {
+        //        raw += heights[x, y].ToString("F2") + " ";
+        //    }
+        //    raw += '\n';
+        //}
+
+        //File.WriteAllText("C:\\UnityProjects\\AI2\\TestOutput\\perlin.txt", raw);
+
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y)
+    {
+        float xCord = (float)x / width * Scale;
+        float yCord = (float)y / height * Scale;
+
+        if (Randomized == true)
+        {
+            xCord += offsetX;
+            yCord += offsetY;
+        }
+
+
+        return Mathf.PerlinNoise(xCord, yCord);
+    }
+}
---
+++ Assets/Scripts/ProcedureArray2D.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ProcedureArray2D : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+
+    public int width;
+    public int height; // Length of the terrain
+    public int depth; // Maximum height of the terrain
+
+    public float h1Amplitude; // The amplitude for horizontal wave with frequency 1
+    public float h2Amplitude; // The amplitude for horizontal wave with frequency 2
+    public float h4Amplitude; // The amplitude for horizontal wave with frequency 4
+    public float h8Amplitude; // The amplitude for horizontal wave with frequency 8
+    public float h16Amplitude; // The amplitude for horizontal wave with frequency 16
+    public float h32Amplitude; // The amplitude for horizontal wave with frequency 32
+    public float v1Amplitude; // The amplitude for vertical wave with frequency 1
+    public float v2Amplitude; // The amplitude for vertical wave with frequency 2
+    public float v4Amplitude; // The amplitude for vertical wave with frequency 4
+    public float v8Amplitude; // The amplitude for vertical wave with frequency 8
+    public float v16Amplitude; // The amplitude for vertical wave with frequency 16
+    public float v32Amplitude; // The amplitude for vertical wave with frequency 32
+
+    public float[] hFrequencies; // The array for horizontal frequencies
+    public float[] hAmplitudes; // The array for horizontal amplitudes
+    public float[] vFrequencies; // The array for vertical frequencies
+    public float[] vAmplitudes; // The array for vertical amplitudes
+    public float[,] map; // The output map
+    public Terrain terrain; // The terrain to be modified
+
+    // Use this for initialization
+    void Start()
+    {
+        hFrequencies = new float[] { 1, 2, 4, 8, 16, 32 };
+        vFrequencies = new float[] { 1, 2, 4, 8, 16, 32 };
+        hAmplitudes = new float[] { h1Amplitude, h2Amplitude, h4Amplitude, h8Amplitude, h16Amplitude, h32Amplitude };
+        vAmplitudes = new float[] { v1Amplitude, v2Amplitude, v4Amplitude, v8Amplitude, v16Amplitude, v32Amplitude };
+
+        map = new float[128, 128];
+
+        GenerateMap();
+
+        //string raw = "";
+        //for (int y = 0; y < width; y++)
+        //{
+
+        //    for (int x = 0; x < width; x++)
+        //    {
+        //        raw += map[x, y].ToString("F2") + " ";
+        //    }
+        //    raw += '\n';
+        //}
+
+        //File.WriteAllText("C:\\UnityProjects\\AI2\\TestOutput\\wave.txt", raw);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    public TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = map.GetLength(0);
+        //terrainData.heightmapResolution = width + 1;
+
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, map);
+        return terrainData;
+    }
+
+    public void SmoothMap() // Lerp values so the map become smoother
+    {
+
+    }
+
+    public void GenerateMap()
+    {
+        for (int i = 0; i < map.GetLength(0); i++) // Fill in vertical arrays
+        {
+            float[] newCombinedWave = CombinedWave(vFrequencies, vAmplitudes);
+
+            for (int k = 0; k < map.GetLength(0); k++)
+            {
+                map[i, k] += newCombinedWave[k];
+            }
+        }
+
+        for (int i = 0; i < map.GetLength(0); i++) // Fill in horizontal arrays
+        {
+            float[] newCombinedWave = CombinedWave(vFrequencies, vAmplitudes);
+
+            for (int k = 0; k < map.GetLength(0); k++)
+            {
+                map[k, i] += newCombinedWave[k];
+            }
+        }
+
+        for (int i = 0; i < map.GetLength(0); i++) // Normalize height map data to a scale of 1
+        {
+            for (int k = 0; k < map.GetLength(0); k++)
+            {
+                map[k, i] /= 6f;
+            }
+        }
+    }
+
+    public float[] CombinedWave(float[] frequencies, float[] amplitudes) // Combining waves of different frequencies
+    {
+        float[] newCombinedWave = new float[map.GetLength(0)];
+
+        for (int i = 0; i < frequencies.Length; i++)
+        {
+            float[] newWave = Noise(frequencies[i]); // For each frequency generate its array
+
+            for (int k = 0; k < map.GetLength(0); k++)
+            {
+                newCombinedWave[k] += amplitudes[i] * newWave[k]; // Adding the current array with its amplitude to the combined result
+            }
+        }
+
+        return newCombinedWave;
+    }
+
+    public float[] Noise(float frequency) // Generate a shifted sine wave 
+    {
+        float[] newWave = new float[map.GetLength(0)];
+        float phaseShift = BetterRandom.betterRandom(0, Mathf.RoundToInt(10000000 * Mathf.PI)) / 10000000f;
+
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            newWave[i] = Mathf.Sin(2 * Mathf.PI * frequency * i / map.GetLength(0) + phaseShift);
+        }
+
+        return newWave;
+    }
+}
